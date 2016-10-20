301ed4072382bf6392a6237a96b4fa69f7107214
Leo <lovexinlingfeiwu@163.com>
2016-10-20 15:34:34 +0800

    项目创建

---
+++ src/main/java/com/ylingtech/kiigo/App.java
@@ -0,0 +1,13 @@
+package com.ylingtech.kiigo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
